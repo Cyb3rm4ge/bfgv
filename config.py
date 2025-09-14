43d9717f98624793fbb89bd5d3ce79b8880d1959
Cyb3rm4ge <gzneed@gmail.com>
2025-09-14 16:25:31 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+﻿BOT_TOKEN = "8385501441:AAG8_i5we8mHswxTlBRNf9XWf8JeNk4wqUs"  # токен ботика
+CHANNEL_URL = "https://t.me/+8_T7EMwytJxlOTky"  # ссылка канала проверка подписки
+CHANNEL_ID = "-1003070774536"  # айди канала для проверки подписки
+VERIF_CHANNEL_ID = "-4859345153"  # айди чата для верификации(обсуждение канала с постбеками)
+SUPP = "http://t.me/GamblingCFG"
+ADMIN_ID =6518832274  # Ваш айди без скобочек
+PROMO = "T232544" # промокод
